37e97e2d4d1e7f99a1917190b79b1456fdca9ae7
nischal <nischal.bhatewara@gmail.com>
2016-12-03 14:11:13 +0530

    Added Brute force method.

--- Q3/q3_bf.py
+++ Q3/q3_bf.py
@@ -1,10 +1,33 @@
+# this is taking too long!
+
 import os
 import time
 
 print("Running", os.path.basename(__file__))
 
+prime_list = []
+
+
+def is_prime(num):
+    print("Prime check for", num)
+    for i in range(2, int(num / 2)):
+        if num % i == 0:
+            return False
+    return True
+
+
+def get_largest(num):
+    print("Checking for", num)
+    largest = 1
+    for i in range(2, num):
+        if num % i == 0 and i not in prime_list and is_prime(i):
+            prime_list.append(i)
+            largest = i
+    return largest
+
 
 def run():
+    print(get_largest(600851475143))
     return
 
 
