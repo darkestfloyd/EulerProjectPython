33f226779068e923357ddb40196ba4f91e1b0424
nischal <nischal.bhatewara@gmail.com>
2016-12-05 12:32:52 +0530

    Added Brute Force implementation

--- Q5/q5_bf.py
+++ Q5/q5_bf.py
@@ -4,12 +4,24 @@ import time
 print("Running", os.path.basename(__file__))
 
 
-def run():
-    return
+def div_by(num, max):
+    for i in range(2, max + 1):
+        if num % i != 0:
+            return False
+    return True
+
+
+def run(nmax=20):
+    test_num = nmax + 1
+    while True:
+        if div_by(test_num, nmax):
+            return test_num
+        else:
+            test_num += 1
 
 
 start_time = time.time()
-run()
+print(run())
 end_time = time.time()
 
 print("Total time: ", (end_time - start_time), "sec")
