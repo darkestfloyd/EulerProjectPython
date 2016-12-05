908327b602a5572b37351b88659f2cfc8e87cfb5
nischal <nischal.bhatewara@gmail.com>
2016-12-05 12:47:59 +0530

    More tests to make code faster

--- Q5/q5_tests.py
+++ Q5/q5_tests.py
@@ -1,8 +1,31 @@
+import time
+
+
+def div_by_2(num, r):
+    if all(num % n for n in range(2, r + 1)):
+        return True
+    return False
+
+
 def div_by(num, r):
-    for i in range(2, r + 1):
-        if num % i != 0:
+    for ele in range(2, r + 1):
+        if num % ele != 0:
             return False
     return True
 
 
-print(div_by(2525, 10))
+start_time = time.time()
+for i in range(1000):
+    div_by(2520, 10)
+
+end_time = time.time()
+
+print("Took", (end_time - start_time))
+
+start_time = time.time()
+for i in range(1000):
+    div_by_2(2520, 10)
+
+end_time = time.time()
+
+print("Took", (end_time - start_time))
