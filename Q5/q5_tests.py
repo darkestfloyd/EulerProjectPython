468a7ed5bac732965248f9168e503f72ed936311
nischal <nischal.bhatewara@gmail.com>
2016-12-05 12:52:01 +0530

    Reverting back to original function
    - "if all" is costly when number is not divisible by small numbers (2, 3, etc)

--- Q5/q5_tests.py
+++ Q5/q5_tests.py
@@ -1,12 +1,3 @@
-import time
-
-
-def div_by_2(num, r):
-    if all(num % n for n in range(2, r + 1)):
-        return True
-    return False
-
-
 def div_by(num, r):
     for ele in range(2, r + 1):
         if num % ele != 0:
@@ -14,18 +5,5 @@ def div_by(num, r):
     return True
 
 
-start_time = time.time()
 for i in range(1000):
-    div_by(2520, 10)
-
-end_time = time.time()
-
-print("Took", (end_time - start_time))
-
-start_time = time.time()
-for i in range(1000):
-    div_by_2(2520, 10)
-
-end_time = time.time()
-
-print("Took", (end_time - start_time))
+    div_by(2521, 10)
