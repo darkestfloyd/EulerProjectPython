5a7bb3b1eb56326680c60d0251eae63751d656f8
nischal <nischal.bhatewara@gmail.com>
2016-12-05 13:22:09 +0530

    Added a recursive alternative

--- Q5/q5_alt.py
+++ Q5/q5_alt.py
@@ -11,7 +11,15 @@ def lcm(a, b):
     return int((a * b) / math.gcd(a, b))
 
 
+def lcm_for_range(counter, multiple, nmax):
+    if counter == nmax:
+        return multiple
+    else:
+        return lcm_for_range(counter + 1, lcm(multiple, counter), nmax)
+
+
 def run(nmax=20):
+    # print(lcm_for_range(1, 1, nmax)) # this is a recursive alternative
     multiple = 1
     for i in range(2, nmax + 1):
         multiple = lcm(multiple, i)
