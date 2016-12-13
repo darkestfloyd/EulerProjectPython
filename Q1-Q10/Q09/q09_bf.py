3571e07caf7b9e6f2ad9519923e8cf806317f41a
nischal <nischal.bhatewara@gmail.com>
2016-12-13 22:46:00 +0530

    Solution to Q09 (Brute force)

---
+++ Q1-Q10/Q09/q09_bf.py
@@ -0,0 +1,21 @@
+import os
+import time
+
+print("Running", os.path.basename(__file__))
+
+
+def run(n=1000):
+    for a in range(1, n):
+        for b in range(1, n):
+            c = (a ** 2 + b ** 2) ** 0.5
+            if a + b + c == n:
+                print(a * b * c)
+                return
+    return
+
+
+start_time = time.time()
+run()
+end_time = time.time()
+
+print("Total time: ", (end_time - start_time), "sec")
