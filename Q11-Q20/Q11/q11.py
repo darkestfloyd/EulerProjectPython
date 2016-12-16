20e63a79670afb8ca3c58b3de6ee4f80472e7c7d
nischal <nischal.bhatewara@gmail.com>
2016-12-16 23:06:19 +0530

    Found solution to Q11

--- Q11-Q20/Q11/q11.py
+++ Q11-Q20/Q11/q11.py
@@ -1,3 +1,5 @@
+# answer - 70600674
+
 import os
 
 import time
@@ -34,10 +36,11 @@ def run():
     values = []
     for i in range(17):
         for j in range(17):
-            down = grid[i][j] * grid[i + 1][j] * grid[i + 2][j] * grid[i + 3][j]
-            left = grid[i][j] * grid[i][j + 1] * grid[i][j + 2] * grid[i][j + 3]
-            diag = grid[i][j] * grid[i + 1][j + 1] * grid[i + 2][j + 2] * grid[i + 3][j + 3]
-            values.append(max([down, left, diag]))
+            values.append(grid[i][j] * grid[i + 1][j] * grid[i + 2][j] * grid[i + 3][j])
+            values.append(grid[i][j] * grid[i][j + 1] * grid[i][j + 2] * grid[i][j + 3])
+            values.append(grid[i][j] * grid[i + 1][j + 1] * grid[i + 2][j + 2] * grid[i + 3][j + 3])
+            if j > 2:
+                values.append(grid[i][j] * grid[i + 1][j - 1] * grid[i + 2][j - 2] * grid[i + 3][j - 3])
     print(max(values))
     return
 
