7c7d9a057d76e3489c48a48dbd5fc3e8d68cd17a
nischal <nischal.bhatewara@gmail.com>
2016-12-26 22:40:24 +0530

    Defined structure of functions and variables

--- Q11-Q20/Q14/q14.py
+++ Q11-Q20/Q14/q14.py
@@ -3,13 +3,25 @@ import time
 
 print("Running", os.path.basename(__file__))
 
+collatz_length = {1: 1}
+
+
+def get_collatz_length(n):
+    return 1
+
 
 def run(limit=1e6):
+    nmax = 0
+    for n in range(int(limit)):
+        test_num = get_collatz_length(n)
+        if test_num > nmax:
+            nmax = test_num
+    print(nmax)
     return
 
 
 start_time = time.time()
-run()
+run(3)
 end_time = time.time()
 
 print("Total time: ", (end_time - start_time), "sec")
