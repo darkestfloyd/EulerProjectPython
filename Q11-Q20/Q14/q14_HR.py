48e54b8c9400a9ebe388d08c8e58b03ea20f4bda
nischal <nischal.bhatewara@gmail.com>
2016-12-27 00:39:56 +0530

    Okay till now!

--- Q11-Q20/Q14/q14_HR.py
+++ Q11-Q20/Q14/q14_HR.py
@@ -4,6 +4,17 @@
 # read input, print length from pre-computer array
 # and no recursion!
 
+collatz_array = [-1] * int(5e6)
+length_array = [0] * int(5e6)
+
+
+def generate_lengths():
+    print("okay")
+    return
+
+
+collatz_array[0] = length_array[0] = 1
+generate_lengths()
 t = int(input().strip())
 for counter in range(t):
     tc = int(input().strip())
