4921944c6499eb81278163bb36567fab73a3a7c1
nischal <nischal.bhatewara@gmail.com>
2016-12-27 00:50:22 +0530

    testing HR

--- Q11-Q20/Q14/q14_HR.py
+++ Q11-Q20/Q14/q14_HR.py
@@ -4,16 +4,34 @@
 # read input, print length from pre-computer array
 # and no recursion!
 
-collatz_array = [-1] * int(5e6)
-length_array = [0] * int(5e6)
+collatz_sequence_length = [-1] * (3 * int(5e6) + 1)
+total_length = [0] * int(5e6)
+
+
+def get_collatz_length(cn):
+    length = 0
+    while collatz_sequence_length[cn] != 1:
+        length += 1
+        if cn % 2 == 0:
+            cn //= 2
+        else:
+            cn = (3 * cn) + 1
+    collatz_sequence_length[cn] = length
+    return length
 
 
 def generate_lengths():
-    print("okay")
+    for human_n in range(2, 10):
+        n = human_n - 1
+        start = total_length[n - 1]
+        nmax = nval = 0
+        for n2 in range(start, n + 1):
+            print(get_collatz_length(n2))
+            pass
     return
 
 
-collatz_array[0] = length_array[0] = 1
+collatz_sequence_length[0] = total_length[0] = 1
 generate_lengths()
 t = int(input().strip())
 for counter in range(t):
