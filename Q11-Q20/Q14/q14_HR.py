1ba8eeb571637f09580e220c6145c7d16deda735
nischal <nischal.bhatewara@gmail.com>
2016-12-27 00:14:33 +0530

    Initial setup

--- Q11-Q20/Q14/q14_HR.py
+++ Q11-Q20/Q14/q14_HR.py
@@ -2,3 +2,8 @@
 # use array not map(faster)
 # pre-compute longest for all values under 5x10^6 + 1
 # read input, print length from pre-computer array
+# and no recursion!
+
+t = int(input().strip())
+for counter in range(t):
+    tc = int(input().strip())
