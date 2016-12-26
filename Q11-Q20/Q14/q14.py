9867693a6b1a04ebaa93963faedbf79ba4f3c1fe
nischal <nischal.bhatewara@gmail.com>
2016-12-26 22:42:54 +0530

    Defined collatz function

--- Q11-Q20/Q14/q14.py
+++ Q11-Q20/Q14/q14.py
@@ -7,7 +7,16 @@ collatz_length = {1: 1}
 
 
 def get_collatz_length(n):
-    return 1
+    if n in collatz_length:
+        return collatz_length[n]
+    else:
+        length = 0
+        if n % 2 == 0:
+            length = get_collatz_length(n / 2)
+        else:
+            length = get_collatz_length((3 * n) + 1)
+        collatz_length[n] = length
+        return length
 
 
 def run(limit=1e6):
