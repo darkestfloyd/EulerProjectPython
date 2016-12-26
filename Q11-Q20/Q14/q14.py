1beb8350787fcdbc78a781d9d779b370c3a724ba
nischal <nischal.bhatewara@gmail.com>
2016-12-27 00:36:53 +0530

    Removed useless  comment

--- Q11-Q20/Q14/q14.py
+++ Q11-Q20/Q14/q14.py
@@ -23,7 +23,7 @@ def get_collatz_length(n):
 def run(limit=1000000):
     nmax = 0
     nval = 0
-    for n in range(1, limit + 1):  # use steps of 2, as even numbers always have lesser length
+    for n in range(1, limit + 1):
         test_num = get_collatz_length(n)
         if test_num >= nmax:
             nmax = test_num
