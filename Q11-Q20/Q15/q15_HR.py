a4c0084b820a75172ec3419a063aba3c93bb1c47
nischal <nischal.bhatewara@gmail.com>
2016-12-31 23:26:34 +0530

    HR accepted (code converted to python2)

--- Q11-Q20/Q15/q15_HR.py
+++ Q11-Q20/Q15/q15_HR.py
@@ -1,20 +1,12 @@
-# score - 20
+# score - 100
+# ATTENTION!! accepted in python2. python3 wrong answer.
+import math
+
+f = math.factorial
+
 
 def run(n=20, m=20):
-    mat = [[0 for x in range(m)] for y in range(n)]
-    mat[n - 1][m - 1] = 1
-    for i in range(n - 1, -1, -1):
-        for j in range(m - 1, -1, -1):
-            if j == m - 1:
-                jp = 1
-            else:
-                jp = mat[i][j + 1]
-            if i == n - 1:
-                ip = 1
-            else:
-                ip = mat[i + 1][j]
-            mat[i][j] = ip + jp
-    print(int(mat[0][0] % (1e9 + 7)))
+    print(int((f(m + n) // (f(m) * f(n))) % 1000000007))
     return
 
 
