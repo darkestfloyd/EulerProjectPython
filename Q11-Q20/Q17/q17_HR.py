f05fe672cad65bc6ef57fa328ba4716434ca05b9
nischal <nischal.bhatewara@gmail.com>
2017-01-03 13:40:02 +0530

    Added words for all required numbers

--- Q11-Q20/Q17/q17_HR.py
+++ Q11-Q20/Q17/q17_HR.py
@@ -1,5 +1,37 @@
 # HR follows US English. No 'and' in words
 
+_0_in_words = 'Zero'
+_1_to_19 = {
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    10: 'Ten',
+    11: 'Eleven',
+    12: 'Twelve',
+    13: 'Thirteen',
+    14: 'Fourteen',
+    15: 'Fifteen',
+    16: 'Sixteen',
+    17: 'Seventeen',
+    18: 'Eighteen',
+    19: 'Nineteen'}
+tens_scale = {
+    20: 'Twenty',
+    30: 'Thirty',
+    40: 'Forty',
+    50: 'Fifty',
+    60: 'Sixty',
+    70: 'Seventy',
+    80: 'Eighty',
+    90: 'Ninety'}
+numeric_scale = {}  # TODO add later according to code
+
 
 def run(n):
     return
