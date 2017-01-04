70b82daef1efb2d8318569438ca2ec292a4d74a8
nischal <nischal.bhatewara@gmail.com>
2017-01-04 20:30:16 +0530

    Some more optimizations

--- Q11-Q20/Q14-incomplete/q14_generator.py
+++ Q11-Q20/Q14-incomplete/q14_generator.py
@@ -3,10 +3,12 @@ largest_collatz_length = {1: 1}
 
 
 def collatz_length(n):
+    temp_n = n
     length = 0
     while n != 1:
         if n in collatz_length_for_n:
-            return collatz_length_for_n[n] + length
+            collatz_length_for_n[temp_n] = collatz_length_for_n[n] + length
+            return collatz_length_for_n[temp_n]
         length += 1
         if n % 2 == 0:
             n //= 2
@@ -28,24 +30,24 @@ def get_largest(upper):
     return 0
 
 
-# print(collatz_length(3))
-for x in range(2, int(5e6)):
-    if x % 1000 == 0:
-        print(x / int(5e6) * 100)
-    get_largest(x)
-
-file = open("q14_generated.py", 'a+')
-
-file.write('largest_collatz_length = {')
-for l in range(1, max(largest_collatz_length) + 1):
-    file.write(str(l) + ":" + str(largest_collatz_length[l]) + ", ")
-
-file.write('}')
-
-file.write("\n\n")
-file.write("t = int(input().strip())\n")
-file.write("for counter in range(t):\n")
-file.write("    tt = int(input().strip())\n")
-file.write("    print(largest_collatz_length[tt])\n\n")
-
-file.close()
+print(collatz_length(3))
+# for x in range(2, int(5e6)):
+#     if x % 1000 == 0:
+#         print(x / int(5e6) * 100)
+#     get_largest(x)
+#
+# file = open("q14_generated.py", 'a+')
+#
+# file.write('largest_collatz_length = {')
+# for l in range(1, max(largest_collatz_length) + 1):
+#     file.write(str(l) + ":" + str(largest_collatz_length[l]) + ", ")
+#
+# file.write('}')
+#
+# file.write("\n\n")
+# file.write("t = int(input().strip())\n")
+# file.write("for counter in range(t):\n")
+# file.write("    tt = int(input().strip())\n")
+# file.write("    print(largest_collatz_length[tt])\n\n")
+#
+# file.close()
