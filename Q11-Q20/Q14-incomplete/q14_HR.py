6ece7c2f9d87d8bba0bb60b9176d9e74616cd674
nischal <nischal.bhatewara@gmail.com>
2017-01-04 21:39:00 +0530

    No improvement in score

--- Q11-Q20/Q14-incomplete/q14_HR.py
+++ Q11-Q20/Q14-incomplete/q14_HR.py
@@ -1,19 +1,37 @@
 # score - 38.46
 
 collatz_length = [-1] * int(5e6)
+collatz = [-1] * (int(5e6) + 1)
 
 
-def get_collatz_length(n):
-    if n < 5e6 and collatz_length[n] != -1:
-        return collatz_length[n]
+# def get_collatz_length(n):
+#     if n < 5e6 and collatz_length[n] != -1:
+#         return collatz_length[n]
+#     else:
+#         if n % 2 == 0:
+#             length = get_collatz_length(n // 2)
+#         else:
+#             length = get_collatz_length((3 * n) + 1)
+#         if n < 5e6:
+#             collatz_length[n] = length + 1
+#         return length + 1
+
+
+def get_collatz_length(n, length=0):
+    # if n < 5e6 and collatz_length[n] != -1:
+    #     return collatz_length[n] + length
+    # else:
+    #     return get_collatz_length(collatz[n], length + 1)
+    if n < 5e6:
+        if collatz_length[n] != -1:
+            return collatz_length[n] + length
+        else:
+            return get_collatz_length(collatz[n], length + 1)
     else:
         if n % 2 == 0:
-            length = get_collatz_length(n // 2)
+            return get_collatz_length(n // 2, length + 1)
         else:
-            length = get_collatz_length((3 * n) + 1)
-        if n < 5e6:
-            collatz_length[n] = length + 1
-        return length + 1
+            return get_collatz_length(3 * n + 1, length + 1)
 
 
 def run(limit=1000000):
@@ -28,6 +46,11 @@ def run(limit=1000000):
     return
 
 
+for col in range(1, int(5e6)):
+    if col % 2 == 0:
+        collatz[col] = col // 2
+    else:
+        collatz[col] = 3 * col + 1
 collatz_length[1] = 0
 t = int(input().strip())
 for counter in range(t):
