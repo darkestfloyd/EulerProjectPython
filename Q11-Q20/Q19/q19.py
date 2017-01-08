c0c4d84bbd2b0714b8bddebeb986499a6d009fa1
nischal <nischal.bhatewara@gmail.com>
2017-01-08 19:48:00 +0530

    Corrected some bugs in date logic

--- Q11-Q20/Q19/q19.py
+++ Q11-Q20/Q19/q19.py
@@ -1,4 +1,4 @@
-, import os
+import os
 import time
 
 print("Running", os.path.basename(__file__))
@@ -15,22 +15,26 @@ def run(date_start=1, date_end=1, month_start=1, month_end=1, year_start=1901, y
         elif yr == year_end:
             ye = True
             months = range(1, month_end + 1)
+        else:
+            months = range(1, 13)
         for month in months:
-            if not ye:
-                if month in [4, 6, 9, 11]:
+            if ye and month == month_end:
+                dmax = date_end
+            else:
+                if month in [3, 5, 8, 10]:
                     dmax = 30
                 elif month == 2:
                     dmax = 29 if yr % 400 == 0 or (yr % 4 == 0 and yr % 100 != 0) else 28
                 else:
                     dmax = 31
-            else:
-                dmax = date_end + 1
 
-            if ys:
-                days = range(date_start, dmax)
+            if ys and month == month_start:
+                days = range(date_start, dmax + 1)
             else:
-                days = range(0, dmax)
+                days = range(1, dmax + 1)
 
+            for x in days:
+                print(x, month, yr)
     # print(no_of_days)
     # print(no_of_days // 365)
     # print(no_of_days % 7)
@@ -38,7 +42,7 @@ def run(date_start=1, date_end=1, month_start=1, month_end=1, year_start=1901, y
 
 
 start_time = time.time()
-run(1, 31, 6, 2, 1901, 1902)
+run(12, 13, 12, 2, 1901, 1903)
 end_time = time.time()
 
 print("Total time: ", (end_time - start_time), "sec")
