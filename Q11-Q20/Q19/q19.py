aaf2cf5e91da8aa486651b985fc140153396c623
nischal <nischal.bhatewara@gmail.com>
2017-01-08 19:23:51 +0530

    Need to calculate for first month only

--- Q11-Q20/Q19/q19.py
+++ Q11-Q20/Q19/q19.py
@@ -4,7 +4,16 @@ import time
 print("Running", os.path.basename(__file__))
 
 
-def run():
+def run(year_start=1901, year_end=2000):
+    no_of_days = 0
+    for yr in range(year_start, year_end + 1):
+        if yr % 4 == 0 and (yr % 100 != 100 or yr % 400 == 0):
+            no_of_days += 1
+        no_of_days += 365
+
+    print(no_of_days)
+    print(no_of_days // 365)
+    print(no_of_days % 7)
     return
 
 
