dd65f194d8c06f3a8c5f9d9742781331e916e3a1
nischal <nischal.bhatewara@gmail.com>
2017-01-09 15:56:08 +0530

    Defined Zeller's

--- Q11-Q20/Q19/q19_HR_alt.py
+++ Q11-Q20/Q19/q19_HR_alt.py
@@ -1,4 +1,15 @@
 # Using Zeller's Congruency
+# h = (q + (13 * (m + 1) / 5) + K + K // 4 + J //4 + 5 * J) mod 7
+# where, h is the day of the week (0 - Sat)
+#        q is day of month
+#        m is the month (3 - March ..., 13 - Jan, 14 - Feb)
+#        K is the year of century (yr mod 100)
+#    and J is yr // 100
+
+
+def run(date_start=1, month_start=1, year_start=1901, date_end=1, month_end=1, year_end=2000):
+    return
+
 
 t = int(input().strip())
 for counter in range(t):
