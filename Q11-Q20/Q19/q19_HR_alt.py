56644a3610b23edac034b65efb58dae38ef8d31c
nischal <nischal.bhatewara@gmail.com>
2017-01-09 16:12:23 +0530

    At least no more timeouts

--- Q11-Q20/Q19/q19_HR_alt.py
+++ Q11-Q20/Q19/q19_HR_alt.py
@@ -1,3 +1,5 @@
+# Score - 28.57
+
 # Using Zeller's Congruency
 # h = (q + (13 * (m + 1) / 5) + K + K // 4 + J //4 + 5 * J) mod 7
 # where, h is the day of the week (0 - Sat)
@@ -8,6 +10,33 @@
 
 
 def run(date_start=1, month_start=1, year_start=1901, date_end=1, month_end=1, year_end=2000):
+    sundays = 0
+    for yr in range(year_start, year_end + 1):
+        # print()
+        if yr == year_start:
+            months = range(month_start, 13)
+        elif yr == year_end:
+            months = range(1, month_end)
+        else:
+            months = range(1, 13)
+        months = [13 if x == 1 else x for x in months]
+        months = [14 if x == 2 else x for x in months]
+
+        for month in months:
+            if yr == year_start and month == month_start and date_start != 1:
+                continue
+            if yr == year_end and month == month_end and date_end <= 1:
+                continue
+            x1 = (13 * (month + 1)) // 5
+            K = yr % 100
+            J = yr // 100
+
+            h = (1 + x1 + K + K // 4 + J // 4 + 5 * J) % 7
+            if h == 1:
+                # print(month, yr)
+                sundays += 1
+
+    print(sundays)
     return
 
 
@@ -17,4 +46,4 @@ for counter in range(t):
     yre, me, de = input().strip().split(' ')
     yrs, ms, ds = [int(yrs), int(ms), int(ds)]
     yre, me, de = [int(yre), int(me), int(de)]
-    # run(ds, ms, yrs, de, me, yre)
+    run(ds, ms, yrs, de, me, yre)
