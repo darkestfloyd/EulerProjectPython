c850038eae05aaa757a4ca5c4a96938d7ab7629c
nischal <nischal.bhatewara@gmail.com>
2017-01-28 23:42:56 +0530

    PE accepted!

--- Q21-Q30/Q22/q22.py
+++ Q21-Q30/Q22/q22.py
@@ -1,3 +1,5 @@
+# answer - 871198282
+
 in_file = open("names.txt", 'r')
 
 names = in_file \
@@ -8,3 +10,16 @@ names = in_file \
 # Algo -
 # First merge sort the names
 # Then convert to number and multiply by the index
+
+names.sort()  # sort the names
+
+name_score = 0
+index = 1
+for name in names:
+    name_sum = 0
+    for ch in name:
+        name_sum += (ord(ch) - 64)
+    name_score += name_sum * index
+    index += 1
+
+print(name_score)
