ad75099775e6791a00ed79d2e969bc76bfa8e216
nischal <nischal.bhatewara@gmail.com>
2017-01-28 19:35:07 +0530

    First attempt

--- Q21-Q30/Q21/q21.py
+++ Q21-Q30/Q21/q21.py
@@ -4,7 +4,26 @@ import time
 print("Running", os.path.basename(__file__))
 
 
-def run():
+def sum_of_d(n):
+    nsum = 0
+    sqrt = n ** 0.5
+    for x in range(1, int(sqrt + 1)):
+        if n % x == 0:
+            nsum += n
+    return nsum
+
+
+def run(nmax=10000):
+    amicable_sum = 0
+    num_flag = [-1] * nmax
+    for a in range(2, nmax):
+        if num_flag[a] != -1:
+            continue
+        b = sum_of_d(a)
+        num_flag[a] = b
+        if a == sum_of_d(b):
+            amicable_sum += (a + b)
+            num_flag[b] = a
     return
 
 
