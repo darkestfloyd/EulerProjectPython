9a48fcac8b3ac5b9f90519b43349aeed7179883e
nischal <nischal.bhatewara@gmail.com>
2017-01-28 21:12:48 +0530

    Score 66.67

--- Q21-Q30/Q21/q21_HR.py
+++ Q21-Q30/Q21/q21_HR.py
@@ -1,11 +1,3 @@
-# answer - 31626
-
-import os
-import time
-
-print("Running", os.path.basename(__file__))
-
-
 def sum_of_divisors(n):
     nsum = 0
     sqrt = int(n ** 0.5)
@@ -19,22 +11,19 @@ def sum_of_divisors(n):
 
 def run(nmax=10000):
     amicable_sum = 0
-    num_flag = [-1] * nmax
     for a in range(2, nmax):
         b = sum_of_divisors(a)
-        num_flag[a] = b
         if b > a:
             c = sum_of_divisors(b)
             if c == a:
-                amicable_sum += a + b
-            if b <= nmax:
-                num_flag[b] = c
+                amicable_sum += a
+                if b <= nmax:
+                    amicable_sum += b
     print(amicable_sum)
     return
 
 
-start_time = time.time()
-run()
-end_time = time.time()
-
-print("Total time: ", (end_time - start_time), "sec")
+t = int(input().strip())
+for counter in range(t):
+    n = int(input().strip())
+    run(n)
