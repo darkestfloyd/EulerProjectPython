fc7fb040c23a062d3b977e4d02b063d85d831aef
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-02-02 17:50:43 +0530

    HR accepted. (Removed HR code)

--- Q21-Q30/Q23/q23_HR.py
+++ Q21-Q30/Q23/q23_HR.py
@@ -1,51 +1,23 @@
-def get_factors(n, proper_divisors=False, sort=True):
-    """
-    Returns factors of n
+# HR accepted! Score 100
 
-    :param n: the number
-    :param proper_divisors: if True, returns only proper factors
-    :param sort: if True, factors are sorted
-    """
-
-    factors = {1}
-    sqrt = int(n ** 0.5)
-    start, steps = [3, 2] if n % 2 else [2, 1]
-    for num in range(start, sqrt + 1, steps):
-        if n % num == 0:
-            factors.add(num)
-            factors.add(n // num)
-
-    if not proper_divisors:
-        factors.add(n)
-
-    if sort:
-        return sorted(factors)
-    else:
-        return factors
-
-
-def sum_of_divisors(n, proper_divisors_only=False):
-    """
-    Returns sum of divisors of n
-
-    :param n: the number
-    :param proper_divisors_only: if True, returns sum of proper divisors
-    """
-    return sum(get_factors(n, proper_divisors_only, sort=False))
-
+from utils.Euler import sum_of_divisors
 
 MAX = 100000
 abundant_numbers = set()
 
 for num in range(12, MAX + 1):
-    if sum_of_divisors(num) > num:
+    if sum_of_divisors(num, True) > num:
         abundant_numbers.add(num)
 
 t = int(input().strip())
 for counter in range(t):
     n = int(input().strip())
+    flag = False
     for a in abundant_numbers:
         if n - a in abundant_numbers:
-            print("YES")
-            exit()
-    print("NO")
+            flag = True
+            break
+    if flag:
+        print("YES")
+    else:
+        print("NO")
