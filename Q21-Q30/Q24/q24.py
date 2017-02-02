5acf2f403959b99741eff32b173fe6ff6e539aa0
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-02-02 18:13:07 +0530

    PE accepted

--- Q21-Q30/Q24/q24.py
+++ Q21-Q30/Q24/q24.py
@@ -1,3 +1,16 @@
+# answer - 2783915460
+
 # Algo - 1. Get permutations of digits
 #        2. Sort the permutations
 #        3. print millionth permutation
+import itertools
+
+MAX = 1000000 - 1
+element = list(next(itertools.islice(itertools.permutations(range(0, 10)), MAX, None)))
+
+num, counter = 0, 0
+for n in element[::-1]:
+    num += (n * (10 ** counter))
+    counter += 1
+
+print(num)
