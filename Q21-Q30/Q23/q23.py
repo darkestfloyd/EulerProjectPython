898e76a78d65c07a4eb7da2a007f09cc5481747b
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-02-02 17:25:55 +0530

    Added logic to get abundant numbers

--- Q21-Q30/Q23/q23.py
+++ Q21-Q30/Q23/q23.py
@@ -2,3 +2,15 @@
 # 1. Get all abundant numbers
 # 2. Add all to get sums and mark the corresponding array to being abundant sum
 # 3. The left out are non-abundant, add them
+
+from utils import Euler
+
+MAX, SUM = 30, 0
+
+abundant_numbers = set()
+
+for n in range(1, MAX + 1):
+    if Euler.sum_of_divisors(n, True) > n:
+        abundant_numbers.add(n)
+
+print(abundant_numbers)
