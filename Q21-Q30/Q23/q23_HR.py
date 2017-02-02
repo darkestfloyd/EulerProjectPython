87d5ebb908d67c24864232cb6a0235cb3b8c49b3
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-02-02 17:41:21 +0530

    Compute abundant numbers till 10^5

--- Q21-Q30/Q23/q23_HR.py
+++ Q21-Q30/Q23/q23_HR.py
@@ -1,7 +1,49 @@
-def run(n):
+def get_factors(n, proper_divisors=False, sort=True):
+    """
+    Returns factors of n
 
+    :param n: the number
+    :param proper_divisors: if True, returns only proper factors
+    :param sort: if True, factors are sorted
+    """
+
+    factors = {1}
+    sqrt = int(n ** 0.5)
+    start, steps = [3, 2] if n % 2 else [2, 1]
+    for num in range(start, sqrt + 1, steps):
+        if n % num == 0:
+            factors.add(num)
+            factors.add(n // num)
+
+    if not proper_divisors:
+        factors.add(n)
+
+    if sort:
+        return sorted(factors)
+    else:
+        return factors
+
+
+def sum_of_divisors(n, proper_divisors_only=False):
+    """
+    Returns sum of divisors of n
+
+    :param n: the number
+    :param proper_divisors_only: if True, returns sum of proper divisors
+    """
+    return sum(get_factors(n, proper_divisors_only, sort=False))
+
+
+def run(n):
     return
 
+
+MAX = 100000
+abundant_numebrs = set()
+
+for num in range(12, MAX + 1):
+    if sum_of_divisors(num) > num:
+        abundant_numebrs.add(num)
 
 t = int(input().strip())
 for counter in range(t):
