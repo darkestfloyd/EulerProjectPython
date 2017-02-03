78c44bfd60a9b9f53bbc363c3c17a2735b2ed895
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-02-03 15:36:49 +0530

    One test case timed out!

--- Q21-Q30/Q25/q25_HR.py
+++ Q21-Q30/Q25/q25_HR.py
@@ -1,13 +1,18 @@
+# score - 66.67
+
 F = [1, 1]
 
-f = 0
-counter = 2
-while f < 10 ** 2:
+f, counter = 0, 2
+while f < 10 ** 5000:
     f = F[counter - 1] + F[counter - 2]
     F.append(f)
     counter += 1
 
-#
-# t = int(input().strip())
-# for counter in range(t):
-#     n = int(input().strip())
+t = int(input().strip())
+for counter in range(t):
+    n, c = int(input().strip()), 1
+    for x in F:
+        if x > 10 ** (n - 1):
+            print(c)
+            break
+        c += 1
