8706bc176934f7a575520519e532b278afae7b23
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-02-03 15:32:59 +0530

    Initial tests pass

--- Q21-Q30/Q25/q25_HR.py
+++ Q21-Q30/Q25/q25_HR.py
@@ -1,8 +1,13 @@
-def run(n):
-    return
+F = [1, 1]
 
+f = 0
+counter = 2
+while f < 10 ** 2:
+    f = F[counter - 1] + F[counter - 2]
+    F.append(f)
+    counter += 1
 
-t = int(input().strip())
-for counter in range(t):
-    n = int(input().strip())
-    run(n)
+#
+# t = int(input().strip())
+# for counter in range(t):
+#     n = int(input().strip())
