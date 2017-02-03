ade9441fb6b2390622dacabac148aaeb54ae1af4
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-02-03 15:08:09 +0530

    Added Q25 file. Skipping Q24 for now

--- Q21-Q30/Q24/q23_HR.py
+++ Q21-Q30/Q24/q23_HR.py
@@ -1,8 +1,15 @@
+import itertools
+
+
 def run(n):
     return
 
 
-t = int(input().strip())
-for counter in range(t):
-    n = int(input().strip())
-    run(n)
+# alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
+# elements = list(itertools.permutations(alphabets))
+
+print(elements[1])
+# t = int(input().strip())
+# for counter in range(t):
+#     n = int(input().strip())
+#     run(n)
