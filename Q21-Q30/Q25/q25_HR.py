4d1823795abc11e663b06cc2bc26ece256eeccc0
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-02-03 15:46:13 +0530

    HR accepted! Score 100

--- Q21-Q30/Q25/q25_HR.py
+++ Q21-Q30/Q25/q25_HR.py
@@ -1,18 +1,18 @@
-# score - 66.67
+# score - 100
 
 F = [1, 1]
+F_digit = {0: 1}
 
-f, counter = 0, 2
-while f < 10 ** 5000:
+f, counter, digit = 0, 2, 1
+while f < 10 ** 20:
     f = F[counter - 1] + F[counter - 2]
     F.append(f)
     counter += 1
+    if f > 10 ** digit:
+        F_digit[digit] = counter
+        digit += 1
 
 t = int(input().strip())
 for counter in range(t):
-    n, c = int(input().strip()), 1
-    for x in F:
-        if x > 10 ** (n - 1):
-            print(c)
-            break
-        c += 1
+    n = int(input().strip())
+    print(F_digit[n - 1])
