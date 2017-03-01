4e0d1b900a96a15f6fb61292aeba3ba89a9cb379
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-03-01 19:40:43 +0530

    Optimized PE solution

--- Q21-Q30/Q28/q28.py
+++ Q21-Q30/Q28/q28.py
@@ -1,19 +1,9 @@
 # answer - 669171001
 
 n = 1001
-N = 2 * (n - 1) + 1
-
-steps = 1
 _sum = 1
-_n = 1
-count = 1
 
-while count < N:
-    f = 2 * _n + 1
-    _sum += f
-    if count % 4 == 0:
-        steps += 1
-    _n += steps
-    count += 1
+for x in range(1, ((n - 1) // 2) + 1):
+    _sum += 4 * (2 * x + 1) ** 2 - 12 * x
 
 print(_sum)
