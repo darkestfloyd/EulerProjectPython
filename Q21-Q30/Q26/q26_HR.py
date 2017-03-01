d1f988eab0a59b7c7aab17ec6215135daa349a73
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-03-01 16:27:52 +0530

    NO improvement

--- Q21-Q30/Q26/q26_HR.py
+++ Q21-Q30/Q26/q26_HR.py
@@ -1,22 +1,32 @@
 t = int(input().strip())
 
+
+def soe(n):
+    sieve = [True] * n
+    sieve[0] = sieve[1] = False
+    sqrt = int(n ** 0.5)
+    for p in range(2, sqrt + 1):
+        if sieve[p]:
+            for x in range(p * p, n, p):
+                sieve[x] = False
+
+    return [x for x in range(n) if sieve[x]]
+
+
+primes = soe(10000)[::-1]
 for _ in range(t):
     d = int(input().strip())
+    if d < 8:
+        print(3)
+        continue
+    x = 0
+    for x in primes:
+        if x > d or x < 7:
+            continue
+        p = 1
+        while pow(10, p, x) != 1 and p < x:
+            p += 1
+        if x - 1 == p:
+            break
 
-    r_max, r_n = 0, 0
-    for num in range(4, d):
-        reminders, l = [-1] * num, 1
-        r = 10 % num
-        reminders[1] = r
-        while reminders[r] == -1 and l < num:
-            n = (r * 10) % num
-            if n == 0:
-                l = 0
-                break
-            reminders[r] = n
-            r = n
-            l += 1
-        if l > r_max:
-            r_max, r_n = l, num
-
-    print(r_n)
+    print(x)
