a8eb5c71e0c4e3ae3f4ad41e9cefcc618fea79dc
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-03-01 19:14:26 +0530

    Passed initial PE tests

--- Q21-Q30/Q28/q28.py
+++ Q21-Q30/Q28/q28.py
@@ -1,1 +1,17 @@
-n = 101
+n = 5
+N = 2 * (n - 1) + 1
+
+steps = 1
+_sum = 1
+_n = 1
+count = 1
+
+while count < N:
+    f = 2 * _n + 1
+    _sum += f
+    if count % 4 == 0:
+        steps += 1
+    _n += steps
+    count += 1
+
+print(_sum)
