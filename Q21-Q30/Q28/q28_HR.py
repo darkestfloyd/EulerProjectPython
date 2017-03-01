3d574eedfadff393ca16e3e092b94818eecca729
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-03-01 19:39:19 +0530

    Optimized HR solution

--- Q21-Q30/Q28/q28_HR.py
+++ Q21-Q30/Q28/q28_HR.py
@@ -2,18 +2,9 @@ t = int(input().strip())
 
 for _ in range(t):
     n = int(input().strip())
-    N = 2 * (n - 1) + 1
-
-    steps = 1
     _sum = 1
-    _n = 1
-    count = 1
 
-    while count < N:
-        _sum += 2 * _n + 1
-        if count % 4 == 0:
-            steps += 1
-        _n += steps
-        count += 1
+    for x in range(1, ((n - 1) // 2) + 1):
+        _sum += 4 * (2 * x + 1) ** 2 - 12 * x
 
     print(_sum % 1000000007)
