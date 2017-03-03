69e1f20ed8f38bee50261df11245323764eb822e
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-03-03 22:51:49 +0530

    PE accepted!

--- Q21-Q30/Q29/q29.py
+++ Q21-Q30/Q29/q29.py
@@ -1,8 +1,10 @@
+# answer - 9183
+
 powers = set()
 
-n_min, n_max = 2, 5
+n_min, n_max = 2, 100
 for a in range(n_min, n_max + 1):
     for b in range(n_min, n_max + 1):
         powers.add(pow(a, b))
 
-print(sorted(powers))
+print(len(powers))
