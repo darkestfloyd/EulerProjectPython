03e5cf4b865125be92c8f9512317ad4bd96da111
NischalBhatewara <nischal.bhatewara@gmail.com>
2017-03-03 22:53:52 +0530

    Passed initial tests

---
+++ Q21-Q30/Q29/q29_HR.py
@@ -0,0 +1,8 @@
+powers = set()
+
+n_min, n_max = 2, int(input().strip())
+for a in range(n_min, n_max + 1):
+    for b in range(n_min, n_max + 1):
+        powers.add(pow(a, b))
+
+print(len(powers))
